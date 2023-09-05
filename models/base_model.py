0e5397bbdd279ea5ff7e5f3a11169062767b3530
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-09-05 11:51:51 +0300

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -10,8 +10,9 @@ class BaseModel():
     created_at = None
     updated_at = None
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         """Initialize the BaseModel"""
+        super().__init__(**kwargs)
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()

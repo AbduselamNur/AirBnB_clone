3f575a9ec5bdf5a56c0ae0db09a6907754a3d745
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-09-06 17:05:52 +0300

    console 0.0.1

---
+++ console.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+a program that contains the entry point of the command interpreter
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """A command interpreter"""
+
+    prompt = "(hbnb) "
+    def do_quit(self, arg):
+        """Quit command to exit the program\n"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program\n"""
+        return True
+    def emptyline(self):
+        """nothing do when + enter"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+

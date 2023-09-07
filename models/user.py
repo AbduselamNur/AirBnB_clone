6036b6d39a1d305f80b5087e9ca2d752b5e2dec0
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-09-07 13:54:02 +0300

    first user

--- models/user.py
+++ models/user.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-"""Defines the User class."""
+"""User Model"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Represent a User.
-
-    Attributes:
-        email (str): The email of the user.
-        password (str): The password of the user.
-        first_name (str): The first name of the user.
-        last_name (str): The last name of the user.
+    """ a class User that inherits from BaseModel
     """
-
     email = ""
     password = ""
     first_name = ""
